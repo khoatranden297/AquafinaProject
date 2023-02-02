25c7cd2b9d9292386e4517e12abe24c5ba0b6a86
khoatranden297 <71304861+khoatranden297@users.noreply.github.com>
2023-02-02 19:51:25 +0700

    upload

--- navigation/HNavigation.js
+++ navigation/HNavigation.js
@@ -1,7 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomePage from '../screens/HomePage';
-import GuideScreen from '../screens/GuideScreen';
+import HuongdanScreen from '../screens/HuongdanScreen';
 import StartScreen from '../screens/StartScreen';
 const Stack = createStackNavigator();
 
@@ -9,8 +9,8 @@ function MyStackNavigation() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Start" component={StartScreen} />
-      <Stack.Screen name="Guide" component={GuideScreen} />
       <Stack.Screen name="HomePage" component={HomePage} />
+      <Stack.Screen name="Huongdan" component={HuongdanScreen} />
     
 
     </Stack.Navigator>
